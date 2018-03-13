c2a18f8ed259aa9687c8c645ee5cb26a243881e8
ryan-keenan <ryan.keenan@udacity.com>
2018-03-13 13:18:37 -0700

    increase default SAFETY_DISTANCE

--- motion_planning.py
+++ motion_planning.py
@@ -115,7 +115,7 @@ class MotionPlanning(Drone):
         self.flight_state = States.PLANNING
         print("Searching for a path ...")
         TARGET_ALTITUDE = 5
-        SAFETY_DISTANCE = 3
+        SAFETY_DISTANCE = 5
 
         self.target_position[2] = TARGET_ALTITUDE
 
